a8557387d9dfebc850cb130d9683efc34a914c05
Tunnells <94391588+Tunnells@users.noreply.github.com>
2022-07-17 13:59:37 +0200

    Update UI-Components.lua

--- UI-Components.lua
+++ UI-Components.lua
@@ -1,16 +1,25 @@
 local Players = game:GetService("Players")
 
+local PlayerX = Players.LocalPlayer
+
 local UIAnimated = loadstring(game:HttpGet("https://raw.githubusercontent.com/Tunnells/V-R-N-F-t-i-N-Q-L-q-r-F/main/UI-Animated.lua"))()
 
 local UIComponents = {}
 UIComponents.__Indeex = UIComponents
+
+local function CalculateDistance(Pos)
+	return PlayerX:DistanceFromCharacter(Pos)
+end
 
 UIComponents.Components = {
 	PlayerESP = {
 		Start = function()
 			for _, Player in pairs(Players:GetPlayers()) do
 				if (Player.Character) then
-					UIAnimated.AddUI("PlayerESP", Player.Character.Head)
+					
+					if (CalculateDistance(PlayerX.Character.Head.Position)) <= 3000 then
+						UIAnimated.AddUI("PlayerESP", Player.Character.Head)						
+					end
 				end
 			end
 		end,
@@ -23,7 +32,10 @@ UIComponents.Components = {
 		Start = function()
 			for _, Zombie in pairs(workspace.Zombies.Mobs:GetChildren()) do
 				if (Zombie) then
-					UIAnimated.AddUI("ZombieESP", Zombie.Head)					
+					
+					if (CalculateDistance(Zombie.Head.Position)) <= 3000 then
+						UIAnimated.AddUI("ZombieESP", Zombie.Head)					
+					end					
 				end
 			end
 		end,
@@ -36,7 +48,11 @@ UIComponents.Components = {
 		Start = function()
 			for _, Loot in pairs(workspace.Loot:GetDescendants()) do
 				if (Loot:IsA("CFrameValue")) then
-					UIAnimated.AddUI("ItemESP", Loot)
+					
+					
+					if (CalculateDistance(Loot.Position)) <= 3000 then
+						UIAnimated.AddUI("ItemESP", Loot)						
+					end
 				end
 			end
 		end,
@@ -49,7 +65,9 @@ UIComponents.Components = {
 		Start = function()
 			for _, Vehicle in pairs(workspace.Vehicles.Spawned:GetChildren()) do
 				if (Vehicle) then
-					UIAnimated.AddUI("VehicleESP", Vehicle.Base)					
+					if (CalculateDistance(Vehicle.Base.Position)) <= 3000 then
+						UIAnimated.AddUI("VehicleESP", Vehicle.Base)							
+					end				
 				end
 			end
 		end,
